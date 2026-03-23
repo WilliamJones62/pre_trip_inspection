759dbf45d98b4e4e789698d191489aac7289e872
WilliamJones62 <williamjones120862@gmail.com>
2026-03-23 13:27:13 -0400

    Corrected linting issues

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -64,7 +64,7 @@ class ChecksController < ApplicationController
     puts "**** params[:deleted_img_ids] = #{params[:deleted_img_ids]}"
     attachments = ActiveStorage::Attachment.where(id: params[:deleted_img_ids])
     attachments.map(&:purge)
-    redirect_to checks_path, notice: "Inspection images successfully deleted.", status: :see_other 
+    redirect_to checks_path, notice: "Inspection images successfully deleted.", status: :see_other
   end
 
   private
@@ -78,25 +78,25 @@ class ChecksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def check_params
-      params.expect(check: [ :origin, :destionation, :trip_date, :truck_tag, :trailer_tag, 
-      :truck_clearance_lights, :headlights, :level, :leaks, :alternator, :alternator_wires, 
-      :water_pump, :wp_hoses, :belt, :air_compressor, :ac_hoses, :coolant_reservoir, 
+      params.expect(check: [ :origin, :destionation, :trip_date, :truck_tag, :trailer_tag,
+      :truck_clearance_lights, :headlights, :level, :leaks, :alternator, :alternator_wires,
+      :water_pump, :wp_hoses, :belt, :air_compressor, :ac_hoses, :coolant_reservoir,
       :cr_hoses, :oil_stick, :steering_reservoir, :sr_hoses, :steering_shaft, :ss_universal_joint,
-      :steering_gearbox, :sg_hoses, :pitman_arm, :tf_leaf_spring_mounts, :tf_leaf_springs, 
-      :tf_u_bolts, :tf_shock_absorbers, :tf_br_hosess, :tf_chambers, :tf_slacks, :tf_shoes, 
-      :tf_drums, :tf_br_tires, :tf_rims, :tf_lugs, :tf_hubs, :tf_valves, :brackets, :tractor_handles, 
-      :tractor_doors, :door_seals, :door_steps, :tankss, :chain, :exhaust, :frame, :driveshaft, 
-      :ds_universal_joint, :tr_br_hosess, :tr_chambers, :tr_slacks, :tr_shoes, :tr_drums, 
-      :tr_leaf_spring_mounts, :tr_leaf_springs, :tr_u_bolts, :tr_shock_absorbers, 
-      :tractor_airbags, :tr_tires, :tr_rims, :tr_lugs, :tr_hubs, :tr_valves, :tr_muds, 
-      :tr_lights, :space, :front_clearance, :front_panels, :side_panels, 
-      :side_dot, :trailer_frame, :gear, :floor, :cross, :trailer_air_lines, :trailer_br_hoses, 
-      :trailer_chambers, :trailer_slacks, :trailer_shoes, :trailer_drums, :trailer_leaf_spring_mounts, 
-      :trailer_leaf_springs, :trailer_u_bolts, :trailer_shock_absorbers, :trailer_airbags, 
-      :trailer_tires, :trailer_rims, :trailer_lugs, :trailer_hubs, :trailer_valves, :trailer_muds, 
-      :abs_light, :rear_clearance, :rear_dot, :trailer_handles, :trailer_doors, :trailer_lights, 
-      :bumper, :coupling_air_lines, :gladhands, :coupling_electric_lines, :apron, :skid_plate, 
-      :pivot_pin, :lock_release, :platform, :kingpin, :chocks, :seat_belt, :cab_abs, :air_govenor, 
+      :steering_gearbox, :sg_hoses, :pitman_arm, :tf_leaf_spring_mounts, :tf_leaf_springs,
+      :tf_u_bolts, :tf_shock_absorbers, :tf_br_hosess, :tf_chambers, :tf_slacks, :tf_shoes,
+      :tf_drums, :tf_br_tires, :tf_rims, :tf_lugs, :tf_hubs, :tf_valves, :brackets, :tractor_handles,
+      :tractor_doors, :door_seals, :door_steps, :tankss, :chain, :exhaust, :frame, :driveshaft,
+      :ds_universal_joint, :tr_br_hosess, :tr_chambers, :tr_slacks, :tr_shoes, :tr_drums,
+      :tr_leaf_spring_mounts, :tr_leaf_springs, :tr_u_bolts, :tr_shock_absorbers,
+      :tractor_airbags, :tr_tires, :tr_rims, :tr_lugs, :tr_hubs, :tr_valves, :tr_muds,
+      :tr_lights, :space, :front_clearance, :front_panels, :side_panels,
+      :side_dot, :trailer_frame, :gear, :floor, :cross, :trailer_air_lines, :trailer_br_hoses,
+      :trailer_chambers, :trailer_slacks, :trailer_shoes, :trailer_drums, :trailer_leaf_spring_mounts,
+      :trailer_leaf_springs, :trailer_u_bolts, :trailer_shock_absorbers, :trailer_airbags,
+      :trailer_tires, :trailer_rims, :trailer_lugs, :trailer_hubs, :trailer_valves, :trailer_muds,
+      :abs_light, :rear_clearance, :rear_dot, :trailer_handles, :trailer_doors, :trailer_lights,
+      :bumper, :coupling_air_lines, :gladhands, :coupling_electric_lines, :apron, :skid_plate,
+      :pivot_pin, :lock_release, :platform, :kingpin, :chocks, :seat_belt, :cab_abs, :air_govenor,
       :leak, :low_air, :pop_out, :trailer_brake, :tractor_brake, :service_brake, :notes, images: [] ])
     end
 end
