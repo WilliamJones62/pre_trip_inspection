357441e872d69db31113cd79af4e8cf1db316f73
WilliamJones62 <williamjones120862@gmail.com>
2026-03-23 15:20:32 -0400

    Commented out all test code

--- test/models/check_test.rb
+++ test/models/check_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+# require "test_helper"
 
-class CheckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class CheckTest < ActiveSupport::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
