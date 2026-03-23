d6a136f86bd0d910c7fccae983c98d95c3a9d48b
WilliamJones62 <williamjones120862@gmail.com>
2026-03-23 13:03:10 -0400

    Added render DB config

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
-# Exit on errorset -o errexit
+# Exit on error
+set -o errexit
 
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
-
-bin/rails db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
